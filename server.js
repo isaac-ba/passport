df554ea91ef6cdfddda277791c1d756165b050b1
Isaac BA <iba.burton@gmail.com>
2016-12-13 14:19:55 +0200

    yay

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ var app = express();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser');
+var expressSession = require('express-session');
 
 app.use(express.static(__dirname));
+
+app.use(expressSession({secret: 'mySecretKey'}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
+
+passport.serializeUser(function (user, done) {
+  console.log(user);
+  done(null, user.myID);
+});
 
 passport.use('login', new LocalStrategy(function (username, password, done) {
   var authenticated = username === "John" && password === "Smith";
@@ -23,8 +31,7 @@ passport.use('login', new LocalStrategy(function (username, password, done) {
 
 app.post('/login', passport.authenticate('login', {
   successRedirect: '/success',
-  failureRedirect: '/login',
-  session: false
+  failureRedirect: '/login'
 }));
 
 app.get('/success', function (req, res){
